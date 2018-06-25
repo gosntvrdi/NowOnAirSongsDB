d29e1f091a63c5d613b195ef5631c524500722bd
luka <father.luka@gmail.com>
2018-06-25 19:30:03 +0200

    bn

--- playlistDbToSongDB.py
+++ playlistDbToSongDB.py
@@ -7,6 +7,6 @@ timestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S')
 conn = mariadb.connect(host='192.168.150.251', user='videostream', database='songsDB')
 cursor = conn.cursor()
 cursor.execute("""INSERT INTO test (songName, youtubeLink, clientID, date) SELECT songName, youtubeLink, clientID, date FROM playlists
-               ON DUPLICATE KEY UPDATE date = VALUES(date), youtubeLink = VALUES(youtubeLink)""")
+               ON DUPLICATE KEY UPDATE date = VALUES(date), youtubeLink = VALUES(youtubeLink), verifiedLink = 1""")
 
 conn.commit()
