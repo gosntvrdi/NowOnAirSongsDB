8b54560c890334d54271f0d01fdc884445a3cab2
luka <father.luka@gmail.com>
2018-06-25 13:31:16 +0200

    bn

--- scratch.py
+++ scratch.py
@@ -8,15 +8,21 @@ import datetime
 import os
 import glob
 
-
+#set working dir
 dirname = os.path.dirname(__file__)
 file = os.path.join(dirname, 'playlists/*.txt')
+
+#get latest playlist.txt in dir
 playlists = glob.glob(file)
 latestPlaylist = max(playlists, key = os.path.getctime)
+latestPlaylist = open(latestPlaylist, encoding='utf-8')
+
+#get rid of 1st line in playlist.txt (header) and strip the /n (new line)
+latestPlaylistSongs = latestPlaylist.readlines()[1:]
+latestPlaylistSongs = [s.rstrip() for s in latestPlaylistSongs]
+latestPlaylistSongs = ([s.replace('Direct File - ', '') for s in latestPlaylistSongs])
 
 
-
-print (latestPlaylist)
 
 
 
