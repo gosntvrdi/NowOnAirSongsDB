b1c0996c2c214a81265410755a0c38535c75ea6a
luka <father.luka@gmail.com>
2018-06-25 13:06:40 +0200

    bn

---
+++ songsDB.py
@@ -0,0 +1,52 @@
+import mysql.connector as mariadb
+import pafy
+import urllib.request
+import urllib.parse
+import re
+import time
+import datetime
+import pyinotify
+import os
+
+
+dirname = os.path.dirname(__file__)
+file = os.path.join(dirname, 'NowOnAir/NowOnAir.txt')
+
+
+def songDB():
+    NowOnAir = open(file, encoding='utf-8').readline()
+    NowOnAir = NowOnAir[7:]
+    query_string = urllib.parse.urlencode({"search_query": NowOnAir})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    try:
+        link = ('http://www.youtube.com/watch?v=' + search_results[0])
+    except IndexError:
+        link = ('https://www.youtube.com/watch?v=l6A4qnAX5Gw')
+    videoPafy = pafy.new(link)
+    best = videoPafy.getbestvideo()
+    videompv = best.url
+    clientID = 'YammatFM'
+    songDB = NowOnAir
+    youtubeLinkDB = videompv
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S')
+    conn = mariadb.connect(host='192.168.150.251', user='videostream', database='songsDB')
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO songsDB (songName, youtubeLink, clientID, date) VALUES (%s, %s, %s, %s)
+                   ON DUPLICATE KEY UPDATE date = VALUES(date)""", (songDB, youtubeLinkDB, clientID, timestamp))
+    conn.commit()
+    conn.close()
+
+
+class ModHandler(pyinotify.ProcessEvent):
+    def process_IN_CLOSE_WRITE(self, evt):
+        songDB()
+
+
+handler = ModHandler()
+wm = pyinotify.WatchManager()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch(file, pyinotify.IN_CLOSE_WRITE)
+notifier.loop()
+
